702d579a4fd5194f970f65b53b6101ca87fcc45e
Duck0-Land <erhilston@gmail.com>
2023-03-08 00:09:51 -0500

    fixed css/html bugs and finished the api functions

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// START GLOBAL VARIABLES //
+//variables start//
 
 var openWeatherApiKey = '26ba3a7e283acb9cd1e8665c6c3b319a';
 var openWeatherCoordinatesUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
@@ -12,45 +12,38 @@ var currentDay = moment().format('M/DD/YYYY');
 const weatherIconUrl = 'http://openweathermap.org/img/wn/';
 var searchHistoryArray = loadSearchHistory();
 
-// END GLOBAL VARIABLES //
+//variables end//
 
-// Define function to capitalize the first letter of a string
+//function that capatalizes the first letter of a string//
 function titleCase(str) {
     var splitStr = str.toLowerCase().split(' ');
     for (var i = 0; i < splitStr.length; i++) {
-        // You do not need to check if i is larger than splitStr length, as your for does that for you
-        // Assign it back to the array
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-    }
-    // Directly return the joined string
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);}
+    //Directly return the joined string
     return splitStr.join(' ');
 }
+// section end//
 
-//load cities from local storage and recreate history buttons
+//local stoage for city search history//
 function loadSearchHistory() {
     var searchHistoryArray = JSON.parse(localStorage.getItem('search history'));
-
-    // if nothing in localStorage, create a new object to track all user info
     if (!searchHistoryArray) {
         searchHistoryArray = {
             searchedCity: [],
         };
     } else {
-        //add search history buttons to page
         for (var i = 0; i < searchHistoryArray.searchedCity.length; i++) {
             searchHistory(searchHistoryArray.searchedCity[i]);
-        }
-    }
-
+        }}
     return searchHistoryArray;
 }
 
-//save to local storage
+//save to local storage//
 function saveSearchHistory() {
     localStorage.setItem('search history', JSON.stringify(searchHistoryArray));
 };
 
-//funciton to create history buttons
+//history buttons//
 function searchHistory(city) {
     var searchHistoryBtn = $('<button>')
         .addClass('btn')
@@ -65,15 +58,14 @@ function searchHistory(city) {
             type: 'button'
         });
 
-    // append btn to search history div
     searchHistoryEl.append(searchHistoryBtn);
 }
 
-//function to get weather data from apiUrl
+
+
+//function to pull data from the weather api//
 function getWeather(city) {
-    // apiUrl for coordinates
     var apiCoordinatesUrl = openWeatherCoordinatesUrl + city + '&appid=' + openWeatherApiKey;
-    // fetch the coordinates for parameter city
     fetch(apiCoordinatesUrl)
         .then(function (coordinateResponse) {
             if (coordinateResponse.ok) {
@@ -82,43 +74,33 @@ function getWeather(city) {
                     var cityLongitude = data.coord.lon;
                     // fetch weather information
                     var apiOneCallUrl = oneCallUrl + cityLatitude + '&lon=' + cityLongitude + '&appid=' + openWeatherApiKey + '&units=imperial';
-
                     fetch(apiOneCallUrl)
                         .then(function (weatherResponse) {
                             if (weatherResponse.ok) {
                                 weatherResponse.json().then(function (weatherData) {
-
-                                    // ** START CURRENT DAY DISPLAY ** //
-
-                                    //add div to hold current day details
                                     var currentWeatherEl = $('<div>')
                                         .attr({
                                             id: 'current-weather'
                                         })
-
-                                    // get the weather icon from city
+                                    //pull the weather icon from the city//
                                     var weatherIcon = weatherData.current.weather[0].icon;
                                     var cityCurrentWeatherIcon = weatherIconUrl + weatherIcon + '.png';
-
-                                    // create h2 to display city + current day + current weather icon
                                     var currentWeatherHeadingEl = $('<h2>')
                                         .text(city + ' (' + currentDay + ')');
-                                    // create img element to display icon
+
                                     var iconImgEl = $('<img>')
                                         .attr({
                                             id: 'current-weather-icon',
                                             src: cityCurrentWeatherIcon,
                                             alt: 'Weather Icon'
                                         })
-                                    //create list of current weather details
+                                    //creates a list of current weather details//
                                     var currWeatherListEl = $('<ul>')
 
                                     var currWeatherDetails = ['Temp: ' + weatherData.current.temp + ' °F', 'Wind: ' + weatherData.current.wind_speed + ' MPH', 'Humidity: ' + weatherData.current.humidity + '%', 'UV Index: ' + weatherData.current.uvi]
 
                                     for (var i = 0; i < currWeatherDetails.length; i++) {
-                                        //create an indiviual list item and append to ul
 
-                                        // run conditional to assign background color to UV index depending how high it is
                                         if (currWeatherDetails[i] === 'UV Index: ' + weatherData.current.uvi) {
 
                                             var currWeatherListItem = $('<li>')
@@ -138,66 +120,51 @@ function getWeather(city) {
                                             }
 
                                             currWeatherListItem.append(uviItem);
-
-                                            //create every list item that isn't uvIndex
                                         } else {
                                             var currWeatherListItem = $('<li>')
                                                 .text(currWeatherDetails[i])
-                                            //append to ul
+                                            //append to ul//
                                             currWeatherListEl.append(currWeatherListItem);
                                         }
 
                                     }
 
-                                    //append curr weather div to col2 before #five-day
+                                    //append curr weather div to col2 before #five-day//
                                     $('#five-day').before(currentWeatherEl);
-                                    //append current weather heading to current weather div
                                     currentWeatherEl.append(currentWeatherHeadingEl);
-                                    //append icon to current weather header
                                     currentWeatherHeadingEl.append(iconImgEl);
-                                    //append ul to current weather
                                     currentWeatherEl.append(currWeatherListEl);
 
-                                    // ** END CURRENT DAY DISPLAY ** //
-
-                                    // ** START 5-DAY FORECAST DISPLAY ** //
-
+                                    // **end display** //
+                                    // **start 5 day forecsat** //
                                     //create h2 header for 5-day forecast
                                     var fiveDayHeaderEl = $('<h2>')
                                         .text('5-Day Forecast:')
                                         .attr({
                                             id: 'five-day-header'
                                         })
-
-                                    //append 5 day forecast header to col2 after current weather div
+                                    //append 5 day forecast header to col2 after current weather div//
                                     $('#current-weather').after(fiveDayHeaderEl)
 
-                                    // create array for the dates for the next 5 days
-
                                     var fiveDayArray = [];
-
                                     for (var i = 0; i < 5; i++) {
                                         let forecastDate = moment().add(i + 1, 'days').format('M/DD/YYYY');
 
                                         fiveDayArray.push(forecastDate);
                                     }
-
-                                    // for each date in the array create a card displaying temp, wind and humidity
+                                    //for each date in the array a card displaying temp, wind and humidity is created//
                                     for (var i = 0; i < fiveDayArray.length; i++) {
-                                        // create a div for each card
                                         var cardDivEl = $('<div>')
                                             .addClass('col3');
 
-                                        // create div for the card body
                                         var cardBodyDivEl = $('<div>')
                                             .addClass('card-body');
 
-                                        // create the card-title
                                         var cardTitleEl = $('<h3>')
                                             .addClass('card-title')
                                             .text(fiveDayArray[i]);
 
-                                        // create the icon for current day weather
+                                        //creates current day weather icon//
                                         var forecastIcon = weatherData.daily[i].weather[0].icon;
 
                                         var forecastIconEl = $('<img>')
@@ -206,17 +173,17 @@ function getWeather(city) {
                                                 alt: 'Weather Icon'
                                             });
 
-                                        // create card text displaying weather details
+                                        //card display weather details//
                                         var currWeatherDetails = ['Temp: ' + weatherData.current.temp + ' °F', 'Wind: ' + weatherData.current.wind_speed + ' MPH', 'Humidity: ' + weatherData.current.humidity + '%', 'UV Index: ' + weatherData.current.uvi]
-                                        //create temp
+
                                         var tempEL = $('<p>')
                                             .addClass('card-text')
                                             .text('Temp: ' + weatherData.daily[i].temp.max)
-                                        //create wind
+
                                         var windEL = $('<p>')
                                             .addClass('card-text')
                                             .text('Wind: ' + weatherData.daily[i].wind_speed + ' MPH')
-                                        // create humidity
+
                                         var humidityEL = $('<p>')
                                             .addClass('card-text')
                                             .text('Humidity: ' + weatherData.daily[i].humidity + '%')
@@ -224,45 +191,35 @@ function getWeather(city) {
 
                                         //append cardDivEl to the #five-day container
                                         fiveDayEl.append(cardDivEl);
-                                        //append cardBodyDivEL to cardDivEl
                                         cardDivEl.append(cardBodyDivEl);
-                                        //append card title to card body
                                         cardBodyDivEl.append(cardTitleEl);
-                                        //append icon to card body
                                         cardBodyDivEl.append(forecastIconEl);
-                                        //append temp details to card body
                                         cardBodyDivEl.append(tempEL);
-                                        //append wind details to card body
                                         cardBodyDivEl.append(windEL);
-                                        //append humidity details to card body
                                         cardBodyDivEl.append(humidityEL);
                                     }
-
-                                    // ** END 5-DAY FORECAST DISPLAY ** //
+                                    // **end 5 day forecast** //
                                 })
                             }
                         })
                 });
-                // if fetch goes through but Open Weather can't find details for city
+                //if fetch goes through but Open Weather can't find details for city//
             } else {
                 alert('Error: Open Weather could not find city')
             }
         })
-        // if fetch fails
+        //if fetch fails//
         .catch(function (error) {
             alert('Unable to connect to Open Weather');
         });
 }
 
-//function to push button elements to 
-
+//function to push button elements//
 function submitCitySearch(event) {
     event.preventDefault();
 
-    //get value from user input
     var city = titleCase(cityInputEl.val().trim());
 
-    //prevent them from searching for cities stored in local storage
     if (searchHistoryArray.searchedCity.includes(city)) {
         alert(city + ' is included in history below. Click the ' + city + ' button to get weather.');
         cityInputEl.val('');
@@ -271,19 +228,19 @@ function submitCitySearch(event) {
         searchHistory(city);
         searchHistoryArray.searchedCity.push(city);
         saveSearchHistory();
-        //empty the form text area
+        //empty the form text area//
         cityInputEl.val('');
         
-        //if user doesn't type in a city
+        //if user doesn't type in a city//
     } else {
         alert('Please enter a city');
     }
 }
 
-// on submission of user data get user input for city and fetch api data
+//on submission of user data get user input for city and fetch api data//
 userFormEL.on('submit', submitCitySearch);
 
-// on click of search button - empty the current weather and 5-day forecast info
+//on click of search button - empty the current weather and 5-day forecast info//
 $('#search-btn').on('click', function () {
     $('#current-weather').remove();
     $('#five-day').empty();
